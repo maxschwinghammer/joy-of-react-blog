78761516a565d57fa27eef7c747dc033d70cb545
Max Schwinghammer <max.schwinghammer@vaillant-group.com>
2026-03-03 08:22:23 +0100

    Exercise 5B

---
+++ src/components/RespectMotionPreferences/RespectMotionPreferences.js
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+import { MotionConfig } from "motion/react";
+
+function RespectMotionPreferences({ children }) {
+  return <MotionConfig reducedMotion="user">{children}</MotionConfig>;
+}
+
+export default RespectMotionPreferences;
